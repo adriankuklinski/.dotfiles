8a7dbce2e077336babfcb227a8fbaf0743c65c4b
Adrian Kuklinski <adrian.kuklinski@eventconnect.io>
2022-11-06 12:31:16 -0500

    refactor: convert init vim to lua and add after/plugin directory to fix timing issue with loading keymaps

---
+++ .config/nvim/after/plugin/keymaps/init.lua
@@ -0,0 +1,24 @@
+local Remap = require("adriankuklinski.keymaps")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
+
+nnoremap("<leader>pv", ":Ex<CR>")
+nnoremap("<leader>u", ":UndotreeShow<CR>")
+
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
+
+xnoremap("<leader>p", "\"_dP")
+
+nnoremap("<leader>y", "\"+y")
+vnoremap("<leader>y", "\"+y")
+nmap("<leader>Y", "\"+Y")
+
+nnoremap("<leader>d", "\"_d")
+vnoremap("<leader>d", "\"_d")
+vnoremap("<leader>d", "\"_d")
+
+inoremap("<C-c>", "<Esc>")
---
+++ .config/nvim/lua/adriankuklinski/init.lua
@@ -0,0 +1,6 @@
+require("adriankuklinski.set")
+require("adriankuklinski.plugins")
+require("adriankuklinski.telescope")
+require("adriankuklinski.lsp")
+require("adriankuklinski.completion")
+require("adriankuklinski.treesitter")
