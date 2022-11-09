93b2d5beaf8aaaedc4ff00fa34ddf9ecb10493b6
Adrian Kuklinski <adrian.kuklinski@eventconnect.io>
2022-11-08 23:04:05 -0500

    feat: add telescope search functions for dotfiles and git branches

--- .config/nvim/lua/adriankuklinski/telescope.lua
+++ .config/nvim/lua/adriankuklinski/telescope.lua
@@ -15,3 +15,25 @@ require('telescope').setup{
 }
 
 require('telescope').load_extension('fzf')
+
+local M = {}
+
+M.search_dotfiles = function()
+	require("telescope.builtin").find_files({
+		prompt_title = "< VimRC >",
+		cwd = vim.env.DOTFILES,
+		hidden = true,
+	})
+end
+
+M.git_branches = function()
+	require("telescope.builtin").git_branches({
+		attach_mappings = function(_, map)
+			map("i", "<c-d>", actions.git_delete_branch)
+			map("n", "<c-d>", actions.git_delete_branch)
+			return true
+		end,
+	})
+end
+
+return M
