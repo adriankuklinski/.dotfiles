6cbf632fe04e8b49c2c2fd01f0d78b4a744099a8
Adrian Kuklinski <adrian.kuklinski@eventconnect.io>
2022-12-03 22:58:57 -0500

    feat: add vim type hover and jump to definition keybinds to lsp setup

--- .config/nvim/lua/adriankuklinski/lsp.lua
+++ .config/nvim/lua/adriankuklinski/lsp.lua
@@ -1,4 +1,9 @@
-require("lspconfig").tsserver.setup({})
+require("lspconfig").tsserver.setup({
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definotion, {buffer=0})
+    end
+})
 
 require("lspconfig").gopls.setup({
 	cmd = { "gopls", "serve" },
@@ -10,9 +15,17 @@ require("lspconfig").gopls.setup({
 			staticcheck = true,
 		},
 	},
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definotion, {buffer=0})
+    end
 })
 
 require("lspconfig").rust_analyzer.setup({
 	cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definotion, {buffer=0})
+    end
 })
 
