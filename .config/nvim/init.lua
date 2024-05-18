0b4a618a5b2c553544a0a0045346339c19afd712
Adrian Kuklinski <akuklinski@tenetos.dev>
2024-05-18 12:04:27 -0400

    feat: Update init with lazy.nvim entry point

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,20 @@
-require('adriankuklinski')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{import = "config"}, 
+	{import = "plugins"}
+})
+
+
